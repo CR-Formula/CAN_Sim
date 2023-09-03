d46fd48902a581358b806a901730c86427ea7d20
Ryan McNab <ryanm05@iastate.edu>
2023-09-02 19:04:28 -0500

    Fixed all errors, should work

--- CAN_Sim/CAN_Sim.ino
+++ CAN_Sim/CAN_Sim.ino
@@ -1,6 +1,8 @@
 #include <mcp_can.h>
 
-void setup() {
+MCP_CAN CAN0(10);     // Initialize CAN
+
+int main(){
   Serial.begin(115200);
   if(CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_16MHZ) == CAN_OK) 
     Serial.println("MCP2515 Initialized Successfully!");
@@ -16,24 +18,25 @@ void setup() {
   int input[6] = {input_packet1, input_packet2, input_packet3, input_packet4, input_packet5, input_packet6};
   for (int i = 0; i < 6; i++)
     loop(i, input[i]); //run function loop with each input of the array
+  return 0;
 }
 
-void loop(int packetNum, int input) {
-  long float packetHexNum = 0x0CFFF048 + (packetNum * 256);
+static void loop(int packetNum, int inputPackets[]) {
+  int packetHexNum = 0x0CFFF048 + (packetNum * 256);
   int highByte[4];
   int lowByte[4];
   for (int i = 0; i < 4; i++){
-    highByte[i] = input[i] / 256; //rpm highByte, tps highByte, etc...
-    lowByte[i] = input[i] % 256; //rpm lowByte, tps lowByte, etc...
+    highByte[i] = inputPackets[i] / 256; //rpm highByte, tps highByte, etc...
+    lowByte[i] = inputPackets[i] % 256; //rpm lowByte, tps lowByte, etc...
   }
-  int output[8]; //rpm lowByte, rpm highByte, etc...
+  byte output[8]; //rpm lowByte, rpm highByte, etc...
   int c = 0; //new counter
   for (int i = 0; i < 4; i++){
     output[c] = lowByte[i]; //fills even numbers of array with lowBytes
     output[c + 1] = highByte[i]; //fills odd numbers of arrays with highBytes
     c + 2;
   }
-  byte sndStat = CAN0.senMsgBuf(packetHexNum, 0, 8, output); //sends output
+  byte sndStat = CAN0.sendMsgBuf(packetHexNum, 0, 8, output); //sends output
   if(sndStat == CAN_OK) //checks if it sends successfully
     Serial.println("Message Sent Successfully!");
   else
