08967228b2dd2fc981c732f347001f8e269538e8
Ryan McNab <ryanm05@iastate.edu>
2023-09-07 21:50:48 -0500

    Fixed to work with arduino

--- CAN_Sim/CAN_Sim.ino
+++ CAN_Sim/CAN_Sim.ino
@@ -1,44 +1,47 @@
 #include <mcp_can.h>
+#include <SPI.h>
 
 MCP_CAN CAN0(10);     // Initialize CAN
 
-int main(){
+int input[6][4] = {
+  {10000, 40, 10, 50},
+  {0, 0, 5, 0},
+  {0, 0, 0, 0},
+  {0, 0, 0, 0},
+  {0, 0, 0, 0},
+  {0, 500, 500, 0}
+};
+
+void setup()
+{
   Serial.begin(115200);
   if(CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_16MHZ) == CAN_OK) 
     Serial.println("MCP2515 Initialized Successfully!");
   else 
     Serial.println("Error Initializing MCP2515...");
-  int input_packet1[4] = {10000, 40, 10, 50};  //rpm, tps, fuel open time, ignition angle
-  int input_packet2[4] = {0, 0, 5, 0};
-  int input_packet3[4] = {0, 0, 0, 0};
-  int input_packet4[4] = {0, 0, 0, 0};
-  int input_packet5[4] = {0, 0, 0, 0};
-  int input_packet6[4] = {0, 500, 500, 0};
-
-  int input[6] = {input_packet1, input_packet2, input_packet3, input_packet4, input_packet5, input_packet6};
-  for (int i = 0; i < 6; i++)
-    loop(i, input[i]); //run function loop with each input of the array
-  return 0;
 }
 
-static void loop(int packetNum, int inputPackets[]) {
-  int packetHexNum = 0x0CFFF048 + (packetNum * 256);
-  int highByte[4];
-  int lowByte[4];
-  for (int i = 0; i < 4; i++){
-    highByte[i] = inputPackets[i] / 256; //rpm highByte, tps highByte, etc...
-    lowByte[i] = inputPackets[i] % 256; //rpm lowByte, tps lowByte, etc...
+void loop() {
+  for(int packetNum = 0; packetNum < 6; packetNum++)
+  {
+    int packetHexNum = 0x0CFFF048 + (packetNum * 256);
+    int highByte[4];
+    int lowByte[4];
+    for (int i = 0; i < 4; i++){
+      highByte[i] = input[packetNum][i] / 256; //rpm highByte, tps highByte, etc...
+      lowByte[i] = input[packetNum][i] % 256; //rpm lowByte, tps lowByte, etc...
+    }
+    byte output[8]; //rpm lowByte, rpm highByte, etc...
+    int c = 0; //new counter
+    for (int i = 0; i < 4; i++){
+      output[c] = lowByte[i]; //fills even numbers of array with lowBytes
+      output[c + 1] = highByte[i]; //fills odd numbers of arrays with highBytes
+      c += 2;
+    }
+    byte sndStat = CAN0.sendMsgBuf(packetHexNum, 0, 8, output); //sends output
+    if(sndStat == CAN_OK) //checks if it sends successfully
+      Serial.println("Message Sent Successfully!");
+    else
+      Serial.println("Error Sending Message...");
   }
-  byte output[8]; //rpm lowByte, rpm highByte, etc...
-  int c = 0; //new counter
-  for (int i = 0; i < 4; i++){
-    output[c] = lowByte[i]; //fills even numbers of array with lowBytes
-    output[c + 1] = highByte[i]; //fills odd numbers of arrays with highBytes
-    c + 2;
-  }
-  byte sndStat = CAN0.sendMsgBuf(packetHexNum, 0, 8, output); //sends output
-  if(sndStat == CAN_OK) //checks if it sends successfully
-    Serial.println("Message Sent Successfully!");
-  else
-    Serial.println("Error Sending Message...");
 }
