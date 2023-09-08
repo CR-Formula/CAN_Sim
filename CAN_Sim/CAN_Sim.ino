9a8bca4f5a57f7881c59fc57ed2d9c091dc288f0
twpowers <twpow@icloud.com>
2023-09-07 22:36:41 -0500

    messeges can now be sent without error

--- CAN_Sim/CAN_Sim.ino
+++ CAN_Sim/CAN_Sim.ino
@@ -17,18 +17,18 @@ void setup() {
       MCP_NORMAL); // Change to normal mode to allow messages to be transmitted
 }
 
-byte data[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+byte data[8];
 
 void packet1(float rpm, float tps, float fuelOpenTime, float ignistionAngle) {
 
-  data[0] = (rpm / 2) / 256;
-  data[1] = (rpm / 2);
-  data[2] = (tps / 2) / 256;
-  data[3] = tps / 2;
-  data[4] = (fuelOpenTime / 2) / 256;
-  data[5] = fuelOpenTime / 2;
-  data[6] = (ignistionAngle / 2) / 256;
-  data[7] = ignistionAngle / 2;
+  data[0] = (rpm / 2);
+  data[1] = (rpm / 2) / 256;
+  data[2] = tps / 2;
+  data[3] = (tps / 2) / 256;
+  data[4] = fuelOpenTime / 2;
+  data[5] = (fuelOpenTime / 2) / 256;
+  data[6] = ignistionAngle / 2;
+  data[7] = (ignistionAngle / 2) / 256;
 }
 
 void packet2(float lam) {
@@ -36,8 +36,8 @@ void packet2(float lam) {
   data[1] = 0;
   data[2] = 0;
   data[3] = 0;
-  data[4] = (lam / 2) / 256;
-  data[5] = (lam / 2);
+  data[4] = (lam / 2);
+  data[5] = (lam / 2) / 256;
   data[6] = 0;
   data[7] = 0;
   data[8] = 0;
@@ -52,10 +52,10 @@ void packet5() {}
 void packet6(float airT, float cT) {
   data[0] = 0;
   data[0] = 0;
-  data[2] = (airT / 2) / 256;
-  data[3] = airT / 2;
-  data[4] = (cT / 2) / 256;
-  data[5] = (cT / 2);
+  data[2] = airT / 2;
+  data[3] = (airT / 2) / 256;
+  data[4] = (cT / 2);
+  data[5] = (cT / 2) / 256;
   data[0] = 0;
   data[0] = 0;
 }
@@ -64,13 +64,14 @@ void packet7() {}
 
 void SendData() {
   packet1(100.000, 28.0000, 25.0000, 15.0000);
-  byte sndStat1 = CAN0.sendMsgBuf(0x0CFF048, 0, 8, data);
-  packet2(63.3000);
-  byte sndStat2 = CAN0.sendMsgBuf(0x0CFF148, 0, 8, data);
-  packet6(56.74000, 71.68000);
-  byte sndStat3 = CAN0.sendMsgBuf(0x0CFF548, 0, 8, data);
+  byte sndStat1 = CAN0.sendMsgBuf(0x000048, 0, 8, data);
+  Serial.println("Sent from Packet");
+  // packet2(63.3000);
+  // byte sndStat2 = CAN0.sendMsgBuf(0x0CFF148, 0, 8, data);
+  // packet6(56.74000, 71.68000);
+  // byte sndStat3 = CAN0.sendMsgBuf(0x0CFF548, 0, 8, data);
 
-  if (sndStat1 == CAN_OK & sndStat2 == CAN_OK & sndStat3 == CAN_OK) {
+  if (sndStat1 == CAN_OK) { //sndStat1 == CAN_OK & sndStat2 == CAN_OK & sndStat3 == CAN_OK
     Serial.println("Messages Sent Successfully!");
   } else {
     Serial.println("Error Sending Messages...");
